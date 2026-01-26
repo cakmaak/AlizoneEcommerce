f401030e281374aa37d04e1f5a00437cc5e5d453
cakmaak <yusuf612844@gmail.com>
2026-01-26 17:53:48 +0300

    updasdasd

--- src/pages/Orders/AddressSelect.jsx
+++ src/pages/Orders/AddressSelect.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAddresses } from "../../features/address/addressSlice";
-import { createOrder as createOrderApi } from "../../services/orderApi";
 import { useNavigate } from "react-router-dom";
 import { MapPin, User, Phone, FileText, Building2, Home, ShoppingCart,Check } from "lucide-react";
 import { fetchCart } from "../../features/cart/cartSlice";
 import { addAddress } from "../../features/address/addressSlice";
 import { Link } from "react-router-dom";
 import { showToast } from "../../components/ui/showToast";
+import { createOrder } from "../../features/order/orderSlice";
+
 
 
 
@@ -55,22 +56,24 @@ const { items: cartItems = [], totalPrice = 0 } = useSelector(
     );
   const handleCreateOrder = async () => {
   if (!selectedAddressId) {
-    showToast("error", "Lütfen bir teslimat adresi seçin");
+    showToast("error", "Adres seç");
     return;
   }
 
   if (!termsAccepted) {
-   showToast("error", "KVKK ve Mesafeli Satış Sözleşmesini kabul etmelisiniz");
+    showToast("error", "KVKK ve sözleşmeleri kabul etmelisiniz");
     return;
   }
 
   try {
     setCreatingOrder(true);
 
-    const result = await createOrderApi({
-      addressId: selectedAddressId,
-      contractsAccepted: true,
-    });
+    const result = await dispatch(
+      createOrder({
+        addressId: selectedAddressId,
+        contractsAccepted: true,
+      })
+    ).unwrap();
 
     const url = new URL(result.paymentLink);
     const orderId = url.searchParams.get("orderId");
@@ -78,7 +81,7 @@ const { items: cartItems = [], totalPrice = 0 } = useSelector(
 
     navigate(`/fakebank?orderId=${orderId}&amount=${amount}`);
   } catch (err) {
-   showToast("error", "Sipariş oluşturulamadı");
+    showToast("error", "Sipariş oluşturulamadı");
   } finally {
     setCreatingOrder(false);
   }
